e3a590468d9cf2da9dcad5313bfff421efe041d9
Jkotoun <Jkotoun58@gmail.com>
2019-12-02 19:18:43 +0100

    basic functionality update

--- Proj3/proj3.c
+++ Proj3/proj3.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 //struktura pro mapu bludiste
-typedef struct {
+typedef struct map{
   int rows;
   int cols;
   unsigned char *cells;
@@ -39,7 +39,7 @@ void map_init(Map *map)
     }
 }
 //kody chyb
-enum ERRORS {ARGS_ERROR = 1, FILE_ERROR};
+enum ERRORS {ARGS_ERROR = 1, FILE_ERROR, FILE_ACCESS_ERROR};
 //Funkce vracejici logickou hodnotu podle toho, jestli je retezec cislo
 bool str_is_num(char *str)
 {
@@ -68,41 +68,61 @@ int main(int argc, char **argv)
             print_help();
             return 0;
     }
-    //pokud je argument test se zadanym souborem
-    else if(argc == 3 && strcmp(argv[1], "--test") == 0)
+    else if(argc > 2)
     {
-        FILE *f = fopen(argv[2],"r");
-        if(f == NULL)
+        bool test_only = false;
+        char *file_name;
+        //program je  spusten pouze jako test validity bludiste v souboru
+        if(strcmp(argv[1], "--test") == 0)
+        {
+            test_only = true;
+            file_name = argv[2];
+        }
+        //program je spusten pro vyhledani cesty v bludisti s validnimi argumenty 
+        else if(argc == 5 && str_is_num(argv[2]) && str_is_num(argv[3]) && (strcmp(argv[1], "--lpath") == 0 || strcmp(argv[1], "--rpath") == 0))
+        {
+            file_name = argv[4];
+        }
+        else
+        {
+            fprintf(stderr, "Invalid arguments\n");
+            return ARGS_ERROR;
+        }
+        //otevreni souboru s bludistem, overeni jestli existuje
+        FILE *maze_file = fopen(file_name,"r");
+        if(maze_file == NULL)
         {
             fprintf(stderr, "Soubor nenalezen\n");
             return FILE_ERROR;
         }
-    }
-    //pokud jsou zadany argumenty pro vyhledavani v bludisti (metoda vyhledavani, pocatecni radek a sloupec a txt soubor)
-    else if(argc == 5 && str_is_num(argv[2]) && str_is_num(argv[3]) && (strcmp(argv[1], "--lpath") == 0 || strcmp(argv[1], "--rpath") == 0))
-    {
-        //otevreni souboru
-        FILE *maze_file = fopen(argv[4], "r");
-        if(maze_file == NULL)
-        {
-            fprintf(stderr, "Soubor nenalezen");
-            return FILE_ERROR;
-        }
-        //nacteni poctu radku a sloupcu ze souboru (2 cisla na prvnim radku)
+        //nacteni poctu radku a sloupcu ze souboru (prvni a druhe cislo na prvnim radku)
         int maze_rows, maze_cols;
-        fscanf(maze_file,"%d %d", &maze_rows, &maze_cols);
-        //alokovani pameti pro mapu a inicializace na pocetaceni hodnoty
+        int loaded_nums = fscanf(maze_file,"%d %d", &maze_rows, &maze_cols);
+        if(loaded_nums == 0)
+        {
+            fprintf(stderr, "Chyba cteni ze souboru");
+            return FILE_ACCESS_ERROR;
+        }
+        //alokovani pameti pro mapu a inicializace na pocatecni hodnoty
         Map maze_map = map_ctor(maze_rows,maze_cols);
         map_init(&maze_map);
-        if(strcmp(argv[1], "--lpath") == 0)
+        //test validity souboru
+        if(test_only)
         {
-            //LPATH
-            printf("lpath\n");
+            //test()
+            return 0;
         }
+        //vyhledani cesty v bludisti
         else
         {
-            //RPATH
-            printf("rpath\n");
+            if(strcmp(argv[1], "--lpath") == 0)
+            {
+                //TODO LPATH()
+            }
+            else
+            {
+                //TODO RPATH()
+            }
         }
         //uvolneni pameti po mapě
         map_dtor(&maze_map);
