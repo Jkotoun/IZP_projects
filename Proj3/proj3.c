b971774ef99dbfb75f4c1150bf639766e2aa94b2
Jkotoun <Jkotoun58@gmail.com>
2019-12-02 17:21:57 +0100

    first commit

--- Proj3/proj3.c
+++ Proj3/proj3.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+typedef struct {
+  int rows;
+  int cols;
+  unsigned char *cells;
+} Map;
 
-int main()
+void print_help()
 {
-
+    printf("--test soubor \n");
+    printf("    otestuje zadany soubor, jestli obsahuje validni definici mapy \n");
+    printf("--rpath  R C soubor.txt \n");
+    printf("    Najde cestu z bludiste zadaneho v souboru 'soubor.txt' z radku R a sloupce C pomoci metody prave ruky\n");
+    printf("--lpath  R C soubor.txt \n");
+    printf("    Najde cestu z bludiste zadaneho v souboru 'soubor.txt' z radku R a sloupce C pomoci metody leve ruky\n");
+}
+int main(int argc, char **argv)
+{
+    if(argc == 2 && strcmp(argv[1], "--help") == 0)
+    {
+            print_help();
+            return 0;
+    }
     
     
     return 0;
