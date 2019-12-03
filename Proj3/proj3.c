63cefc866a39dc8d39ee712a8d4145bdb62765d4
Jkotoun <Jkotoun58@gmail.com>
2019-12-03 18:06:45 +0100

    rpath pathfinding

--- Proj3/proj3.c
+++ Proj3/proj3.c
@@ -4,20 +4,28 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-
-
 //kody chyb
-enum ERRORS {ARGS_ERROR = 1, FILE_ERROR, MAP_LOADING_ERROR};
+enum ERRORS
+{
+    ARGS_ERROR = 1,
+    FILE_ERROR,
+    MAP_LOADING_ERROR
+};
 
 //kody hranic policek
-enum BORDERS {LEFT = 1, RIGHT, UP_DOWN = 4};
+enum BORDERS
+{
+    BORDER_LEFT = 1,
+    BORDER_RIGHT,
+    BORDER_TOP_BOT = 4
+};
 //struktura pro mapu bludiste
-typedef struct map{
-  int rows;
-  int cols;
-  unsigned char *cells;
+typedef struct map
+{
+    int rows;
+    int cols;
+    unsigned char *cells;
 } Map;
-
 
 //konstruktor mapy - alokace pameti, nastaveni hodnoty radku a sloupcu
 Map map_ctor(int rows, int cols)
@@ -40,35 +48,47 @@ void map_dtor(Map *map)
 //inicializace vychozich hodnot na alokovanou pamet
 void map_init(Map *map)
 {
-    for(int i=0;i<(map->cols * map->rows);i++)
+    for (int i = 0; i < (map->cols * map->rows); i++)
     {
         map->cells[i] = 0;
     }
 }
 
+
+
+//je na dane hranici stena? - rozhoduje podle jednotlivych bitu cisel ze souboru, jako hodnoty border predpoklada binarni hodnoty dane hranice (1|2|4)
+
+bool isborder(Map *map, int r, int c, int border)
+{
+    char cell = map->cells[r * map->cols + c];
+    int cell_val = cell - '0';
+    return (border & cell_val) == border;
+}
+
+//nacitani mapy ze souboru do datove struktury map
 int map_load(Map *map, FILE *f)
 {
     char cell_info;
     int row_pos = 0;
     int col_pos = 0;
     int success;
-    while((success = (fscanf(f, "%c", &cell_info))) != EOF)
+    while ((success = (fscanf(f, "%c", &cell_info))) != EOF)
     {
-        if(success==0)
+        if (success == 0)
         {
             return MAP_LOADING_ERROR;
         }
-        if (cell_info != ' ' && cell_info != '\n' &&cell_info != '\r')
+        if (cell_info != ' ' && cell_info != '\n' && cell_info != '\r')
         {
-            if(row_pos >= map->rows || col_pos >= map->cols)
+            if (row_pos >= map->rows || col_pos >= map->cols)
             {
                 return MAP_LOADING_ERROR;
             }
-            if(!isdigit(cell_info))
+            if (!isdigit(cell_info))
                 return MAP_LOADING_ERROR;
             map->cells[map->cols * row_pos + col_pos] = cell_info;
             col_pos++;
-            if(col_pos > map->cols-1)
+            if (col_pos > map->cols - 1)
             {
                 col_pos = 0;
                 row_pos++;
@@ -77,72 +97,258 @@ int map_load(Map *map, FILE *f)
     }
     return 0;
 }
-//je na dane hranici stena? - rozhoduje podle jednotlivych bitu cisel ze souboru, jako hodnoty border predpoklada binarni hodnoty dane hranice (1|2|4)
-bool isborder(Map *map, int r, int c, int border)
-{
-    char cell = map ->cells[r * map->cols + c];
-    int cell_val = cell - '0';
-    return (border & cell_val) == border; 
-}
 
-bool isvalid_map(Map *map)
+void l_pathfinding(Map *map, int r, int c, int start_border)
 {
-    for(int i=0;i<map->rows;i++)
+    int last_step_x = 1;
+    int last_step_y = 0;
+    int current_row = r-1;
+    int current_col = c-1;
+    int next_step_x = 0, next_step_y = 0;
+    while (current_row < map->rows && current_col < map->cols)
     {
-        for(int j=0;j<map->cols;j++)
+        //otoceny trojuhelnik
+        if ((current_col % 2) == (current_row % 2))
         {
-            //je popis pro dane policko
-            if(map->cells[i * map->cols + j] == 0)
-                return false;
-            //prava strana ma shodnou hranici s vedlejsim trojuhelnikem
-            if(j+1 !=map->cols)
+            if (last_step_x == 1)
             {
-                if(isborder(map,i,j,RIGHT) != isborder(map,i,j+1,LEFT))
+                if (!isborder(map, current_row, current_col, BORDER_RIGHT))
                 {
-                    return false;   
+                    next_step_y = 0;
+                    next_step_x = 1;
                 }
-                    
-            }
-            //leva strana ma shodnou hranici s vedlejsim trojuhelnikem
-            if(j!=0)
-            {
-                if(isborder(map,i,j,LEFT) != isborder(map,i,j-1,RIGHT))
-                    return false;
-            }
-                 //shoda spodni / horni hranice, sude a liche podle indexu od 0
-                //horni hranice - sudy sloupec a sudy radek nebo lichy sloupec a lichy radek
-                if(i % 2 == j % 2)
+                else if (!isborder(map, current_row, current_col, BORDER_TOP_BOT))
                 {
-                    if(i!=0)
-                    {
-                        if(isborder(map,i,j,UP_DOWN) != isborder(map,i-1,j,UP_DOWN))
-                            return false;
-                    }
+                    next_step_y = -1;
+                    next_step_x = 0;
                 }
-                //sudy radek lichy sloupec nebo lichy sloupec sudy radek - maji spodni hranici
                 else
                 {
-                    if(i+1!= map->rows)
-                    {
-                        if(isborder(map,i,j,UP_DOWN) != isborder(map,i+1,j,UP_DOWN))
-                        return false;
-                    }
-                 }
+                    next_step_y = 0;
+                    next_step_x = -1;
+                }
+            }
+            else if (last_step_x == -1)
+            {
+                if (!isborder(map, current_row, current_col, BORDER_TOP_BOT))
+                {
+                    next_step_y = -1;
+                    next_step_x = 0;
+                }
+                else if (!isborder(map, current_row, current_col, BORDER_LEFT))
+                {
+                    next_step_y = 0;
+                    next_step_x = -1;
+                }
+                else
+                {
+                    next_step_y = 0;
+                    next_step_x = 1;
+                }
+            }
+            else if (last_step_y == 1)
+            {
+                if (!isborder(map, current_row, current_col, BORDER_LEFT))
+                {
+                    next_step_y = 0;
+                    next_step_x = -1;
+                }
+                else if (!isborder(map, current_row, current_col, BORDER_RIGHT))
+                {
+                    next_step_y = 0;
+                    next_step_x = 1;
+                }
+                else
+                {
+                    next_step_y = -1;
+                    next_step_x = 0;
+                }
+            }
         }
+        //normalni trojuhelnik
+        else
+        {
 
+                if (last_step_x == 1)
+                {
+                    if (!isborder(map, current_row, current_col, BORDER_TOP_BOT))
+                    {
+                        next_step_y = 1;
+                        next_step_x = 0;
+                    }
+                    else if (!isborder(map, current_row, current_col, BORDER_RIGHT))
+                    {
+                        next_step_y = 0;
+                        next_step_x = 1;
+                    }
+                    else
+                    {
+                        next_step_y = 0;    
+                        next_step_x = -1;
+                    }
+                }
+                else if (last_step_x == -1)
+                {
+                    if (!isborder(map, current_row, current_col, BORDER_LEFT))
+                    {
+                        next_step_y = 0;
+                        next_step_x = -1;
+                    }
+                    else if (!isborder(map, current_row, current_col, BORDER_TOP_BOT))
+                    {
+                        next_step_y = 1;
+                        next_step_x = 0;
+                    }
+                    else
+                    {
+                        next_step_y = 0;
+                        next_step_x = 1;
+                    }
+                }
+                else if (last_step_y == -1)
+                {
+                    if (!isborder(map, current_row, current_col, BORDER_RIGHT))
+                    {
+                        next_step_y = 0;
+                        next_step_x = 1;
+                    }
+                    else if (!isborder(map, current_row, current_col, BORDER_LEFT))
+                    {
+                        next_step_y = 0;
+                        next_step_x = -1;
+                    }
+                    else
+                    {
+                        next_step_y = 1;
+                        next_step_x = 0;
+                    }
+                }
+            
+        }
+        printf("%d,%d \n", current_row + 1, current_col + 1);
+        last_step_x = next_step_x;
+        last_step_y = next_step_y;
+        current_col += next_step_x;
+        current_row += next_step_y;
+    }
+}
+
+
+//overeni validity mapy, vraci logickou hodnotu
+bool isvalid_map(Map *map)
+{
+    for (int i = 0; i < map->rows; i++)
+    {
+        for (int j = 0; j < map->cols; j++)
+        {
+            //je popis pro dane policko
+            if (map->cells[i * map->cols + j] == 0 || (map->cells[i * map->cols + j] < '0' || map->cells[i * map->cols + j] > '9'))
+                return false;
+            //prava strana ma shodnou hranici s vedlejsim trojuhelnikem
+            if (j + 1 != map->cols)
+            {
+                if (isborder(map, i, j, BORDER_RIGHT) != isborder(map, i, j + 1, BORDER_LEFT))
+                {
+                    return false;
+                }
+            }
+            //leva strana ma shodnou hranici s vedlejsim trojuhelnikem
+            if (j != 0)
+            {
+                if (isborder(map, i, j, BORDER_LEFT) != isborder(map, i, j - 1, BORDER_RIGHT))
+                    return false;
+            }
+            //shoda spodni / horni hranice, sude a liche podle indexu od 0
+            //horni hranice - sudy sloupec a sudy radek nebo lichy sloupec a lichy radek
+            if (i % 2 == j % 2)
+            {
+                if (i != 0)
+                {
+                    if (isborder(map, i, j, BORDER_TOP_BOT) != isborder(map, i - 1, j, BORDER_TOP_BOT))
+                        return false;
+                }
+            }
+            //sudy radek lichy sloupec nebo lichy sloupec sudy radek - maji spodni hranici
+            else
+            {
+                if (i + 1 != map->rows)
+                {
+                    if (isborder(map, i, j, BORDER_TOP_BOT) != isborder(map, i + 1, j, BORDER_TOP_BOT))
+                        return false;
+                }
+            }
+        }
     }
     return true;
 }
+/*
+int start_border(Map *map, int r, int c, int leftright)
+{
+    //nejedna se o kraj bludiste
+    if(r != 0 && c!= 0 && c!= map->cols -1 && r!=map->rows -1)
+    {
+        return -1;
+    }
+    if(leftright == LEFT_HAND)
+    {
 
-//mazeprint - test function 
+    }
+    else
+    {
+        //levy sloupec
+        if(c == 0)
+        {
+            //prvni policko
+            if(r==0)
+            {
+                //vstup zleva
+                if(isborder(map,r,c,BORDER_TOP_BOT)
+                {
+                    return BORDER_RIGHT;
+                }
+                 //vstup shora
+                else
+                {
+                    return BORDER_LEFT
+                }
+                
+            }
+            //spoledni policko
+            else if(r == map->rows -1)
+            {
+
+                return BORDER_LEFT;
+            }
+            
+
+        }
+        //lichy radek, levy sloupec
+        else if (r%2!=0 && c==0)
+        {
+            if(isborder(map,r,c,BORDER_TOP_BOT)
+            {
+                return BORDER_RIGHT;
+            }
+            //vstup shora
+            else
+            {
+                return BORDER_LEFT;
+            }
+        }
+                
+    }
+    
+}
+*/
+//mazeprint - test function
 void maze_print(Map *map)
 {
     int col_pos = 0;
-    int row_pos  = 0;
+    int row_pos = 0;
 
-    for(int i=0;i<map->cols * map->rows;i++)
+    for (int i = 0; i < map->cols * map->rows; i++)
     {
-        if(col_pos == map->cols)
+        if (col_pos == map->cols)
         {
             printf("\n");
             col_pos = 0;
@@ -150,17 +356,16 @@ void maze_print(Map *map)
         }
         printf(" %c ", map->cells[row_pos * map->cols + col_pos]);
         col_pos++;
-            
-    }    
+    }
     printf("\n");
 }
 
 //Funkce vracejici logickou hodnotu podle toho, jestli je retezec cislo
 bool str_is_num(char *str)
 {
-    for(int i=0;str[i]!='\0';i++)
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        if(!isdigit(str[i]))
+        if (!isdigit(str[i]))
             return false;
     }
     return true;
@@ -178,24 +383,24 @@ void print_help()
 int main(int argc, char **argv)
 {
     //pokud je argument napoveda
-    if(argc == 2 && strcmp(argv[1], "--help") == 0)
+    if (argc == 2 && strcmp(argv[1], "--help") == 0)
     {
-            print_help();
-            return 0;
+        print_help();
+        return 0;
     }
-    else if(argc > 2)
+    else if (argc > 2)
     {
-        
+
         bool test_only = false;
         char *file_name;
         //program je  spusten pouze jako test validity bludiste v souboru
-        if(strcmp(argv[1], "--test") == 0)
+        if (strcmp(argv[1], "--test") == 0)
         {
             test_only = true;
             file_name = argv[2];
         }
-        //program je spusten pro vyhledani cesty v bludisti s validnimi argumenty 
-        else if(argc == 5 && str_is_num(argv[2]) && str_is_num(argv[3]) && (strcmp(argv[1], "--lpath") == 0 || strcmp(argv[1], "--rpath") == 0))
+        //program je spusten pro vyhledani cesty v bludisti s validnimi argumenty
+        else if (argc == 5 && str_is_num(argv[2]) && str_is_num(argv[3]) && (strcmp(argv[1], "--lpath") == 0 || strcmp(argv[1], "--rpath") == 0))
         {
             file_name = argv[4];
         }
@@ -205,35 +410,35 @@ int main(int argc, char **argv)
             return ARGS_ERROR;
         }
         //otevreni souboru s bludistem, overeni jestli existuje
-        FILE *maze_file = fopen(file_name,"r");
-        if(maze_file == NULL)
+        FILE *maze_file = fopen(file_name, "r");
+        if (maze_file == NULL)
         {
             fprintf(stderr, "Soubor nenalezen\n");
             return FILE_ERROR;
         }
         //nacteni poctu radku a sloupcu ze souboru (prvni a druhe cislo na prvnim radku)
         int maze_rows, maze_cols;
-        int loaded_nums = fscanf(maze_file,"%d %d [\r][\n]", &maze_rows, &maze_cols);
-        if(loaded_nums == 0)
+        int loaded_nums = fscanf(maze_file, "%d %d [\r][\n]", &maze_rows, &maze_cols);
+        if (loaded_nums == 0)
         {
             fprintf(stderr, "Chyba cteni ze souboru");
             return FILE_ERROR;
         }
         //alokovani pameti pro mapu a inicializace na pocatecni hodnoty
-        Map maze_map = map_ctor(maze_rows,maze_cols);
+        Map maze_map = map_ctor(maze_rows, maze_cols);
         map_init(&maze_map);
 
-        int load_result = map_load(&maze_map,maze_file);
-        if(load_result == MAP_LOADING_ERROR)
+        int load_result = map_load(&maze_map, maze_file);
+        if (load_result == MAP_LOADING_ERROR)
         {
             fprintf(stderr, "Nepodarilo se nacist mapu");
             return MAP_LOADING_ERROR;
         }
         //test validity souboru
-        if(test_only)
+        if (test_only)
         {
             bool valid = isvalid_map(&maze_map);
-            if(valid)
+            if (valid)
             {
                 printf("Valid\n");
             }
@@ -241,19 +446,19 @@ int main(int argc, char **argv)
             {
                 printf("Invalid\n");
             }
-            
             return 0;
         }
         //vyhledani cesty v bludisti
         else
         {
-            if(strcmp(argv[1], "--lpath") == 0)
+            if (strcmp(argv[1], "--lpath") == 0)
             {
                 //TODO LPATH()
             }
             else
             {
-                //TODO RPATH()
+                l_pathfinding(&maze_map,atoi(argv[2]),atoi(argv[3]), 4);
+
             }
         }
         //uvolneni pameti po mapě
